b785846f8e4a3a0c5d98c4017bfe4315145c3850
junhaesung <seong0428@gmail.com>
2026-06-03 18:26:31 +0900

    feat: 글자수 방어로직 추가

--- src/main/kotlin/com/muma/melon/MelonService.kt
+++ src/main/kotlin/com/muma/melon/MelonService.kt
@@ -17,8 +17,16 @@ class MelonService(
                 playlistId = track.playlistId,
                 position = track.position,
                 melonSongId = track.melonSongId,
-                title = track.title,
-                artistsText = track.artistsText,
+                title = if (track.title.length >= 100) {
+                    track.title.take(100)
+                } else {
+                    track.title
+                },
+                artistsText = if (track.artistsText.length >= 100) {
+                    track.artistsText.split(",").take(2).joinToString(",")
+                } else {
+                    track.artistsText
+                },
                 melonArtistIds = track.melonArtistIds,
                 albumTitle = track.albumTitle,
                 melonAlbumId = track.melonAlbumId,
@@ -30,8 +38,18 @@ class MelonService(
     }
 
     private fun searchTopTracks(title: String, artists: String): List<SpotifyTrack> {
+        val titleText = if (title.length >= 100) {
+            title.take(100)
+        } else {
+            title
+        }
+        val artistsText = if (artists.length >= 100) {
+            artists.split(",").take(2).joinToString(",")
+        } else {
+            artists
+        }
         val trackIds = spotifyClient.search(
-            query = "track:$title artist:$artists",
+            query = "track:$titleText artist:$artistsText",
             type = "track",
             limit = 2,
         ).tracks?.items?.take(2)?.map { it.id } ?: return emptyList()
